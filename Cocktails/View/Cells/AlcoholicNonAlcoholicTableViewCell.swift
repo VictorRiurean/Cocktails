6f79b18397aa92e5e829fc939e39ba83a144a63a
Victor Riurean <victor.riurean@gmail.com>
2021-07-05 15:12:54 +0200

    Changed Alcoholic and Non Alcoholic buttons

--- Cocktails/View/Cells/AlcoholicNonAlcoholicTableViewCell.swift
+++ Cocktails/View/Cells/AlcoholicNonAlcoholicTableViewCell.swift
@@ -31,30 +31,37 @@ class AlcoholicNonAlcoholicTableViewCell: UITableViewCell {
         
         let bounds = UIScreen.main.bounds
         
-        let alcoholicLabel = UILabel()
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: bounds.width - 40, height: 100))
+        
+        let alcoholicLabel = UILabel(frame: CGRect(x: view.bounds.width * 0.15, y: 37.5, width: 50, height: 20))
         alcoholicLabel.text = "Alcoholic"
         alcoholicLabel.textColor = .white
-        let nonAlcoholicLabel = UILabel()
+        alcoholicLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        alcoholicLabel.sizeToFit()
+        let nonAlcoholicLabel = UILabel(frame: CGRect(x: view.bounds.width * 0.62, y: 37.5, width: 50, height: 20))
         nonAlcoholicLabel.text = "Non Alcoholic"
         nonAlcoholicLabel.textColor = .white
+        nonAlcoholicLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        nonAlcoholicLabel.sizeToFit()
         
         let alcoholicButton = UIButton(frame: CGRect(x: 5, y: 5, width: (bounds.width - 10) / 2, height: 90))
         alcoholicButton.setImage(UIImage(named: "alcoholic"), for: .normal)
         alcoholicButton.setTitle("Alcoholic", for: .normal)
         alcoholicButton.clipsToBounds = true
         alcoholicButton.addTarget(self, action: #selector(didTouchAlcoholicButton), for: .touchUpInside)
-        alcoholicButton.addSubview(alcoholicLabel)
-        alcoholicButton.bringSubviewToFront(alcoholicLabel)
         
         let nonAlcoholicButton = UIButton(frame: CGRect(x: bounds.width / 2 + 5 , y: 5, width: (bounds.width - 20) / 2, height: 90))
         nonAlcoholicButton.setImage(UIImage(named: "nonAlcoholic"), for: .normal)
         nonAlcoholicButton.clipsToBounds = true
         nonAlcoholicButton.addTarget(self, action: #selector(didTouchAlcoholicButton), for: .touchUpInside)
-        nonAlcoholicButton.addSubview(nonAlcoholicLabel)
-        nonAlcoholicButton.bringSubviewToFront(nonAlcoholicLabel)
         
-        self.addSubview(alcoholicButton)
-        self.addSubview(nonAlcoholicButton)
+        view.addSubview(alcoholicButton)
+        view.addSubview(nonAlcoholicButton)
+        view.addSubview(alcoholicLabel)
+        view.addSubview(nonAlcoholicLabel)
+        view.bringSubviewToFront(alcoholicLabel)
+        view.bringSubviewToFront(nonAlcoholicLabel)
+        self.addSubview(view)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
